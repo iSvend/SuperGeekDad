fe5a96b50149363b0ea98e436fa81aed885820fd
Svend Knutsen <sk@svendknutsen.com>
2016-02-02 20:45:47 -0700

    more design
    colors and fonts

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
 	<?php wp_head(); ?>
 </head>
 
