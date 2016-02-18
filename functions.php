d016f0f39b0bf83c9f66d9dba16d0b58bb72a1b0
Svend Knutsen <sk@svendknutsen.com>
2016-02-18 07:38:49 -0700

    Added FB open graph function

--- functions.php
+++ functions.php
@@ -251,4 +251,36 @@ function logout_here($logouturl, $redir)
 $redir = get_option('siteurl');
 return $logouturl . '&redirect_to='.get_permalink();
 }
+
+/**
+ * Facebook Open Graphf
+ **/
+ function add_opengraph_doctype( $output ) {
+		return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+	}
+add_filter('language_attributes', 'add_opengraph_doctype');
+
+//Lets add Open Graph Meta Info
+
+function insert_fb_in_head() {
+	global $post;
+	if ( !is_singular()) //if it is not a post or a page
+		return;
+        //echo '<meta property="fb:admins" content="YOUR USER ID"/>';
+        echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+        echo '<meta property="og:type" content="article"/>';
+        echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+        echo '<meta property="og:site_name" content="Super Geek Dad"/>';
+	if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+		$default_image="http://www.supergeekdad.com/wp-content/uploads/2016/02/WIN_20160203_06_42_34_Pro-300x168.jpg"; //replace this with a default image on your server or an image in your media library
+		echo '<meta property="og:image" content="' . $default_image . '"/>';
+	}
+	else{
+		$thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+		echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+	}
+	echo "
+";
+}
+add_action( 'wp_head', 'insert_fb_in_head', 5 );
 ?>
